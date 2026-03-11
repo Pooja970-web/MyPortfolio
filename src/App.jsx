958cada1d32cb6c02c4b8f2248cc873977af2db8
Pooja970-web <poojklh15@gmail.com>
2026-03-11 14:03:22 +0530

    fix hero import path and build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Navbar/Hero/Hero'
-import About from './Components/About/About'
-import Contact from './Components/Contact/Contact'
-
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Navbar/Hero/Hero";
+import About from "./Components/About/About";
+import MyWork from "./Components/MyWork/MyWork";
+import Contact from "./Components/Contact/Contact";
 import Services from './Components/Services/Services'
-import MyWork from './Components/Mywork/Mywork.jsx'
+
 
 
 const App = () => {
